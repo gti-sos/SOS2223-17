b3437c20131545ae56f21ca62288b6977e403f88
Salim Chaieri Chafai <salcha2@alum.us.es>
2023-04-10 11:05:47 +0200

    ffff08

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    <div class="page-container">
+    <style>
+        
+        
+        .page-container {
+          margin: 0 50px; /* aplicar margen de 50px en los laterales */
+        }
+  
+            
+      </style>
+<section>
+        <h2>Team</h2>
+        <ul>
+            <li><a href="https://github.com/salcha2">Salim Chaieri Chafai</a></li>
+            <li><a href="https://github.com/Joseg0">Jose Germinal Garrucho Orozco</a></li>
+            <li><a href="https://github.com/sanpinand">Santiago Pineda Andrade</a></li>
+        </ul>
+
+        <h2>Project description</h2>
+        <p>Our source of information is oriented to the use of bikes,number
+         of self-employed people and number of emergency calls in Andalusia.</p>
+
+        <h2>Repository</h2>
+        <ul>
+            <li>
+                <a href="https://github.com/gti-sos/SOS2223-17">gti-sos/SOS2223-17</a>
+            </li>
+        </ul>
+
+        <h2>URL</h2>
+        <ul>
+            <li>
+                <a href="https://sos2223-17.ew.r.appspot.com/">Pagina Web</a>
+            </li>
+        </ul>
+        <h2>APIs</h2>
+        <ul>
+            <li><a href="https://sos2223-17.ew.r.appspot.com/api/v1/andalusian-bicycle-plans/docs">/andalusian-bicycle-plans/api/v1/docs</a>(developed by<a href="https://github.com/salcha2">Salim Chaieri Chafai</a>)</li>
+            <li><a href="https://sos2223-17.ew.r.appspot.com/api/v2/andalusian-bicycle-plans/docs">/andalusian-bicycle-plans/api/v2/docs</a>(developed by<a href="https://github.com/salcha2">Salim Chaieri Chafai</a>)</li>
+
+            <li><a href=https://sos2223-17.ew.r.appspot.com/api/v1/self-employed-stats/docs>/self-employed-stats/docs</a>(developed by<a href="https://github.com/Joseg0">Jose Germinal Garrucho Orozco</a>)</li>
+            <li><a href="https://sos2223-17.ew.r.appspot.com/api/v1/emergency-call-stats/docs">/emergency-call-stats/docs</a>(developed by<a href="https://github.com/sanpinand">Santiago Pineda Andrade</a>)</li>
+        </ul>
+
+        <h2>Frontends</h2>
+        <ul>
+            <li><a href="https://sos2223-17.ew.r.appspot.com/andalusian-bicycle-plans">/andalusian-bicycle-plans</a>(developed by<a href="https://github.com/salcha2">Salim Chaieri Chafai</a>)</li>
+            <li><a href=https://sos2223-17.ew.r.appspot.com/self-employed-stats>/self-employed-stats</a>(developed by<a href="https://github.com/Joseg0">Jose Germinal Garrucho Orozco</a>)</li>
+            <li><a href="https://sos2223-17.ew.r.appspot.com/emergency-call-stats">/emergency-call-stats</a>(developed by<a href="https://github.com/sanpinand">Santiago Pineda Andrade</a>)</li>
+        </ul>
+    </section>
+</div>
+</body>
