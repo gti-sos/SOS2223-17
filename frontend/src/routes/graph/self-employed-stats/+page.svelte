6a97500c8d75398ce8170671edf8d339b3c37f9d
josgaroro1 <josgaroro1@alum.us.es>
2023-05-05 10:33:22 +0200

    efforts carpeta

--- frontend/src/routes/graph/self-employed-stats/+page.svelte
+++ frontend/src/routes/graph/self-employed-stats/+page.svelte
@@ -47,7 +47,8 @@
         let jaen = graphData.reduce((acumulador, elemento) => { if (elemento.territory === "Jaén") { return acumulador + elemento.value; } return acumulador; }, 0);
         let almeria = graphData.reduce((acumulador, elemento) => { if (elemento.territory === "Almería") { return acumulador + elemento.value; } return acumulador; }, 0);
         let cordoba = graphData.reduce((acumulador, elemento) => { if (elemento.territory === "Córdoba") { return acumulador + elemento.value; } return acumulador; }, 0);
-
+        let total = sevilla + huelva + malaga + granada + jaen + almeria + cordoba;
+        
         Highcharts.chart('container', {
         chart: {
             type: 'column'
@@ -58,7 +59,7 @@
         },
         subtitle: {
             align: 'center',
-            text: 'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+            text: 'Fuente GitHub: <a href="https://github.com/gti-sos/SOS2223-17" target="_blank">gti-sos/SOS2223-17</a>'
         },
         accessibility: {
             announceNewData: {
@@ -82,14 +83,14 @@
                 borderWidth: 0,
                 dataLabels: {
                     enabled: true,
-                    format: '{point.y:.1f}'
+                    format: '{point.y:.1f}%'
                 }
             }
         },
 
         tooltip: {
             headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}</b><br/>'
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b>del total<br/>'
         },
 
         series: [
@@ -99,263 +100,39 @@
                 data: [
                     {
                         name:"Sevilla",
-                        y: sevilla
+                        y: sevilla/total*100
                     },
                     {
                         name: "Huelva",
-                        y: huelva
+                        y: huelva/total*100
                     },
                     {
                         name:"Almería",
-                        y: almeria
+                        y: almeria/total*100
                     },
                     {
                         name:"Córdoba",
-                        y: cordoba
+                        y: cordoba/total*100
                     },
                     {
                         name:"Málaga",
-                        y: malaga
+                        y: malaga/total*100
                     },
                     {
                         name:"Jaén",
-                        y: jaen
+                        y: jaen/total*100
                     },
                     {
                         name:"Granada",
-                        y: granada
+                        y: granada/total*100
                     }
                 ]
 
             }
         ],
-        drilldown: {
-            breadcrumbs: {
-                position: {
-                    align: 'right'
-                }
-            },
-            series: [
-                {
-                    name: 'Chrome',
-                    id: 'Chrome',
-                    data: [
-                        [
-                            'v65.0',
-                            0.1
-                        ],
-                        [
-                            'v64.0',
-                            1.3
-                        ],
-                        [
-                            'v63.0',
-                            53.02
-                        ],
-                        [
-                            'v62.0',
-                            1.4
-                        ],
-                        [
-                            'v61.0',
-                            0.88
-                        ],
-                        [
-                            'v60.0',
-                            0.56
-                        ],
-                        [
-                            'v59.0',
-                            0.45
-                        ],
-                        [
-                            'v58.0',
-                            0.49
-                        ],
-                        [
-                            'v57.0',
-                            0.32
-                        ],
-                        [
-                            'v56.0',
-                            0.29
-                        ],
-                        [
-                            'v55.0',
-                            0.79
-                        ],
-                        [
-                            'v54.0',
-                            0.18
-                        ],
-                        [
-                            'v51.0',
-                            0.13
-                        ],
-                        [
-                            'v49.0',
-                            2.16
-                        ],
-                        [
-                            'v48.0',
-                            0.13
-                        ],
-                        [
-                            'v47.0',
-                            0.11
-                        ],
-                        [
-                            'v43.0',
-                            0.17
-                        ],
-                        [
-                            'v29.0',
-                            0.26
-                        ]
-                    ]
-                },
-                {
-                    name: 'Firefox',
-                    id: 'Firefox',
-                    data: [
-                        [
-                            'v58.0',
-                            1.02
-                        ],
-                        [
-                            'v57.0',
-                            7.36
-                        ],
-                        [
-                            'v56.0',
-                            0.35
-                        ],
-                        [
-                            'v55.0',
-                            0.11
-                        ],
-                        [
-                            'v54.0',
-                            0.1
-                        ],
-                        [
-                            'v52.0',
-                            0.95
-                        ],
-                        [
-                            'v51.0',
-                            0.15
-                        ],
-                        [
-                            'v50.0',
-                            0.1
-                        ],
-                        [
-                            'v48.0',
-                            0.31
-                        ],
-                        [
-                            'v47.0',
-                            0.12
-                        ]
-                    ]
-                },
-                {
-                    name: 'Internet Explorer',
-                    id: 'Internet Explorer',
-                    data: [
-                        [
-                            'v11.0',
-                            6.2
-                        ],
-                        [
-                            'v10.0',
-                            0.29
-                        ],
-                        [
-                            'v9.0',
-                            0.27
-                        ],
-                        [
-                            'v8.0',
-                            0.47
-                        ]
-                    ]
-                },
-                {
-                    name: 'Safari',
-                    id: 'Safari',
-                    data: [
-                        [
-                            'v11.0',
-                            3.39
-                        ],
-                        [
-                            'v10.1',
-                            0.96
-                        ],
-                        [
-                            'v10.0',
-                            0.36
-                        ],
-                        [
-                            'v9.1',
-                            0.54
-                        ],
-                        [
-                            'v9.0',
-                            0.13
-                        ],
-                        [
-                            'v5.1',
-                            0.2
-                        ]
-                    ]
-                },
-                {
-                    name: 'Edge',
-                    id: 'Edge',
-                    data: [
-                        [
-                            'v16',
-                            2.6
-                        ],
-                        [
-                            'v15',
-                            0.92
-                        ],
-                        [
-                            'v14',
-                            0.4
-                        ],
-                        [
-                            'v13',
-                            0.1
-                        ]
-                    ]
-                },
-                {
-                    name: 'Opera',
-                    id: 'Opera',
-                    data: [
-                        [
-                            'v50.0',
-                            0.96
-                        ],
-                        [
-                            'v49.0',
-                            0.82
-                        ],
-                        [
-                            'v12.1',
-                            0.14
-                        ]
-                    ]
-                }
-            ]
-        }
     });
     }
+    
     onMount(async () => {
         getData();
     });
@@ -363,13 +140,14 @@
 
 </script>
 <main>
-    <h1>Gráfico Autónomos</h1>
+    <h1 style="text-align:center">Gráfico Autónomos</h1>
+
     <figure class="highcharts-figure">
         <div id="container"></div>
         <p class="highcharts-description">
-            Chart showing browser market shares. Clicking on individual columns
-            brings up more detailed data. This chart makes use of the drilldown
-            feature in Highcharts to easily switch between datasets.
+            Gráfico que muestra un porcentaje por provincia andaluza.
+            Cada porcentaje es del total de valor de las empresas
+            autónomas en la comunidad.
         </p>
     </figure>
 </main>
