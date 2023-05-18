5e24d42571de94dfd6acc202dade24934a49c985
josgaroro1 <josgaroro1@alum.us.es>
2023-05-18 22:46:01 +0200

    videos

---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+
+    import { Button } from "sveltestrap";
+
+    async function videoJose(){
+        window.location.href = "https://drive.google.com/file/d/1jXUB8qD3PH6aOxbU1eybUwhl3-U9qQ1o/view?usp=share_link";
+    }
+
+    async function videoSalim(){
+        window.location.href = "";
+    }
+
+    async function videoSanti(){
+        window.location.href = "https://youtu.be/JiIXF10oxWw";
+    }
+
+</script>
+
+<main>
+    <h1>VIDEO JOSE</h1>
+    <Button color="dark" on:click={videoJose}>Ver vídeo</Button>
+    <h1>VIDEO SALIM</h1>
+    <Button color="dark" on:click={videoSalim}>Ver vídeo</Button>
+    <h1>VIDEO SANTIAGO</h1>
+    <Button color="dark" on:click={videoSanti}>Ver vídeo</Button>
+</main>
