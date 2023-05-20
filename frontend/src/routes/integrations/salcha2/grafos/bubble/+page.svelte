00a6c94077b5caf0f050bb654b48d6673529fd22
Salim Chaieri Chafai <salcha2@alum.us.es>
2023-05-20 19:40:19 +0200

    grafico burbujas

--- frontend/src/routes/integrations/salcha2/grafos/bubble/+page.svelte
+++ frontend/src/routes/integrations/salcha2/grafos/bubble/+page.svelte
@@ -1,146 +1,63 @@
 <svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/highcharts-more.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </svelte:head>
 
-<script>
-    // @ts-nocheck
-    import { onMount } from 'svelte';
-    import Highcharts from 'highcharts/highcharts';
-    import HighchartsMore from 'highcharts/highcharts-more';
-    import HighchartsExporting from 'highcharts/modules/exporting';
-    import HighchartsExportData from 'highcharts/modules/export-data';
-    import HighchartsAccessibility from 'highcharts/modules/accessibility';
-    import { dev } from '$app/environment';
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { dev } from '$app/environment';
 
-    let API = '/api/v1/andalusian-bicycle-plans';
+  let API = '/api/v1/andalusian-bicycle-plans';
 
-    if (dev) {
-        API = 'http://localhost:8080' + API;
+  if (dev)
+    API = 'http://localhost:8080' + API;
+
+  let data = [];
+
+  async function getData() {
+    try {
+      const res = await fetch(API);
+      if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+      const dataReceived = await res.json();
+      data = dataReceived;
+      loadChart(data);
+    } catch (error) {
+      console.error(`Error fetching data: ${error}`);
     }
-
-    let data = [];
-    let result = "";
-    let resultStatus = "";
-
-    async function getData() {
-  try {
-    const response = await fetch(API, {
-      method: 'GET'
-    });
-
-    if (!response.ok) {
-      throw new Error('Error en la solicitud');
-    }
-
-    const dataReceived = await response.json();
-    result = JSON.stringify(dataReceived, null, 2);
-    data = dataReceived;
-    loadChart(data);
-  } catch (error) {
-    console.error('Error al obtener los datos:', error);
-    resultStatus = 'Error';
   }
-}
 
-
-    /**
-     * @param {any[]} graphData
-     */
-    function loadChart(graphData) {
-        const options = Highcharts.getOptions();
-        const darkUnica = options.chart && options.chart.style && options.chart.style.name === 'dark-unica' ? options.chart.style : undefined;
-        Highcharts.chart('container', {
-            chart: {
-                type: 'bubble',
-                plotBorderWidth: 1,
-                style: darkUnica
-            },
-            title: {
-                text: 'Highcharts bubbles with radial gradient fill',
-                align: 'left',
-            },
-            xAxis: {
-              title: {
-                text: 'Porcentaje de Uso de Vehículos Motorizados',
-              },
-                gridLineWidth: 1,
-                accessibility: {
-                    rangeDescription: 'Range: 0 to 100.'
-                }
-            },
-            yAxis: {
-                title: {
-        text: 'Uso de Bicicletas',
-      },
-                startOnTick: false,
-                endOnTick: false,
-                accessibility: {
-                    rangeDescription: 'Range: 0 to 100.'
-                }
-            },
-            series: [{
-                type: 'bubble',
-                data: graphData.map((obj) => ({
-                    x: obj.motorized_percentage,
-                    y: obj.bicycle,
-                    z: obj.population,
-                    marker: {
-                        radius: Math.sqrt(obj.population) / 3,
-                    }
-                })),
-                marker: {
-                    fillColor: {
-                        radialGradient: { cx: 0.4, cy: 0.3, r: 0.7 },
-                        stops: [
-                            [0, 'rgba(255,255,255,0.5)'],
-                            [1, Highcharts.color((options.colors || [])[1] || '#000000').setOpacity(0.5).get('rgba').toString()]
-                        ]
-                    }
-                }
-            }, {
-                type: 'bubble',
-                data: graphData.map((obj) => ({
-                    x: obj.motorized_percentage,
-                    y: obj.population,
-                    z: obj.bicycle,
-                    marker: {
-                        radius: obj.walking * 10,
-                    }
-                })),
-                marker: {
-                    fillColor: {
-                        radialGradient: { cx: 0.4, cy: 0.3, r: 0.7 },
-                        stops: [
-                            [0, 'rgba(255,255,255,0.5)'],
-                            [1, Highcharts.color((options.colors || [])[2] || '#000000').setOpacity(0.5).get('rgba').toString()]
-                        ]
-                    }
-                }
-            }]
-        });
+  function loadChart(data: any[]) {
+    if (typeof google === 'undefined') {
+      console.error('Google Charts library is not loaded');
+      return;
     }
 
-    onMount(async () => {
-        HighchartsMore(Highcharts);
-        HighchartsExporting(Highcharts);
-        HighchartsExportData(Highcharts);
-        HighchartsAccessibility(Highcharts);
-        await getData();
+    google.charts.setOnLoadCallback(() => {
+      const chartData = [['Provincia', 'Caminando', 'En Bicicleta']];
+      
+      data.forEach(item => {
+        chartData.push([item.province, item.walking, item.bicycle]);
+      });
+
+      const options: google.visualization.BubbleChartOptions = {
+        title: 'Comparación de Propiedades en Diferentes Provincias',
+        legend: { position: 'top' },
+        chartArea: { width: '80%', height: '70%' },
+        hAxis: { title: 'Caminando' },
+        vAxis: { title: 'En Bicicleta' },
+        bubble: { textStyle: { fontSize: 11 } }
+      };
+
+      const chartContainer = document.getElementById('chart');
+      if (!chartContainer) return;
+      const chart = new google.visualization.BubbleChart(chartContainer);
+      chart.draw(google.visualization.arrayToDataTable(chartData), options);
     });
+  }
+
+  onMount(() => {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(getData);
+  });
 </script>
 
-<main>
-    <h1>Gráfico de Burbujas</h1>
-    <figure class="highcharts-figure">
-        <div id="container"></div>
-        <p class="highcharts-description">
-          Gráfico de burbujas que muestra la relación entre el porcentaje de uso de vehículos motorizados y el uso de bicicletas.
-        </p>
-    </figure>
-
-   
-</main>
+<div id="chart" style="height: 800px;"></div>
