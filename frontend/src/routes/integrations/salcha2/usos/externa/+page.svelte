b0be2b5fa9b94c615234f28a346aa5fb18c8c16f
Salim Chaieri Chafai <salcha2@alum.us.es>
2023-05-20 17:33:39 +0200

    trabajo

---
+++ frontend/src/routes/integrations/salcha2/usos/externa/+page.svelte
@@ -0,0 +1,70 @@
+<script>
+  import { onMount } from 'svelte';
+  import Chart from 'chart.js/auto';
+
+  let repositories = [];
+
+  onMount(async () => {
+    try {
+      const response = await fetch('https://api.github.com/search/repositories?q=stars:%3E1&sort=stars&order=desc&per_page=5');
+      const data = await response.json();
+
+      repositories = data.items.map((repo) => ({
+        name: repo.name,
+        stars: repo.stargazers_count
+      }));
+
+      renderChart();
+    } catch (error) {
+      console.error("Error al obtener los datos:", error);
+    }
+  });
+
+  function renderChart() {
+    const ctx = document.getElementById('chart').getContext('2d');
+    const labels = repositories.map((repo) => repo.name);
+    const data = repositories.map((repo) => repo.stars);
+
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Estrellas',
+          data: data,
+          backgroundColor: [
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(255, 205, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)'
+          ],
+          borderColor: 'rgba(255, 255, 255, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
+</script>
+
+<style>
+  main {
+    max-height: 400px;
+    overflow-y: auto;
+  }
+</style>
+
+<main>
+  <h1>Gráfico de Repositorios Populares en GitHub</h1>
+  <canvas id="chart" style="width: 400px; height: 300px;"></canvas>
+
+  <ul>
+    {#each repositories as repo}
+      <li>{repo.name} - {repo.stars} estrellas</li>
+    {/each}
+  </ul>
+</main>
---
+++ frontend/src/routes/integrations/salcha2/usos/externa1/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { onMount } from 'svelte';
+    import Chart from 'chart.js/auto';
+  
+    let repositories = [];
+  
+    onMount(async () => {
+      try {
+        // Autenticación OAuth con GitHub
+        const clientId = '7091a208d8dc789ceb95';
+        const redirectUri = '819adef573a839f75078614057d4530817be3ac4';
+        const scopes = 'repo';
+  
+        // Generar URL de autorización
+        const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes}`;
+  
+        // Redirigir al usuario a la página de autorización de GitHub
+        window.location.href = authUrl;
+      } catch (error) {
+        console.error("Error al obtener los datos:", error);
+      }
+    });
+  </script>
+  
+  <main>
+    <h1>Autorizando con GitHub...</h1>
+  </main>
+  
